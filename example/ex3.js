bcb2e795a061ee4376524b76e810b1654ec13732
you21979 <you2197901@gmail.com>
2016-04-01 15:38:11 +0900

    mod

---
+++ example/ex3.js
@@ -0,0 +1,15 @@
+"use strict"
+var okcoin = require('okcoin.com');
+var api = okcoin.PublicApi;
+var OrderBook = require('..');
+
+
+api.spot.depth("btc_usd", 60).then(function(depth){
+    var okcoinMapper = function(row){ return [row[0], row[1]] }
+    return new OrderBook(depth.asks.map(okcoinMapper), depth.bids.map(okcoinMapper));
+}).then(function(book){
+    console.log(book.first())
+    console.log(book.last())
+})
+
+
