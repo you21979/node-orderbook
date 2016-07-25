f3f9b39b49c1bde291ee46a9d409edad22d0f72a
you21979 <you2197901@gmail.com>
2016-07-25 17:31:07 +0900

    add distribution map

---
+++ example/ex5.js
@@ -0,0 +1,27 @@
+"use strict"
+var poloniex = require('@you21979/poloniex.com');
+var bittrex = require('@you21979/bittrex.com');
+
+var OrderBook = require('..');
+
+var getPoloniexDepth = function(base, counter){
+    var api = poloniex.PublicApi;
+    return api.orderBook([counter,base].join('_'), 200).then(function(book){
+        var mapper = function(row){ return [row[0], row[1]] }
+        return new OrderBook(book.asks.map(mapper), book.bids.map(mapper), "POLONIEX");
+    })
+}
+var getBittrexDepth = function(base, counter){
+    var api = bittrex.PublicApi;
+    return api.getOrderBook([counter,base].join('-'), 200).then(function(book){
+        var mapper = function(row){ return [row.Rate, row.Quantity] }
+        return new OrderBook(book.sell.map(mapper), book.buy.map(mapper), "BITTREX");
+    })
+}
+
+getPoloniexDepth("XRP","BTC").then(function(book){ return book.dmap() }).then(function(book){
+    var asks = Object.keys(book.asks).map(function(k){ return [(book.asks[k].val).toFixed(8), book.asks[k].vol] })
+    var bids = Object.keys(book.bids).map(function(k){ return [(book.bids[k].val).toFixed(8), book.bids[k].vol] })
+    return new OrderBook(asks, bids, "POLONIEX");
+}).then(console.log)
+//getBittrexDepth("XRP","BTC").then(function(book){ return book.dmap() }).then(console.log)
