0f9029733cd58c6276e8f5e5e6a672a8ab273aaf
you21979 <you2197901@gmail.com>
2016-07-25 17:40:06 +0900

    mod

--- example/ex5.js
+++ example/ex5.js
@@ -19,9 +19,14 @@ var getBittrexDepth = function(base, counter){
     })
 }
 
-getPoloniexDepth("XRP","BTC").then(function(book){ return book.dmap() }).then(function(book){
+var map = [1,2,3,4,5,10,15,20,25,30]
+getPoloniexDepth("XRP","BTC").then(function(book){ return book.dmap(map) }).then(function(book){
     var asks = Object.keys(book.asks).map(function(k){ return [(book.asks[k].val).toFixed(8), book.asks[k].vol] })
     var bids = Object.keys(book.bids).map(function(k){ return [(book.bids[k].val).toFixed(8), book.bids[k].vol] })
     return new OrderBook(asks, bids, "POLONIEX");
 }).then(console.log)
-//getBittrexDepth("XRP","BTC").then(function(book){ return book.dmap() }).then(console.log)
+getBittrexDepth("XRP","BTC").then(function(book){ return book.dmap(map) }).then(function(book){
+    var asks = Object.keys(book.asks).map(function(k){ return [(book.asks[k].val).toFixed(8), book.asks[k].vol] })
+    var bids = Object.keys(book.bids).map(function(k){ return [(book.bids[k].val).toFixed(8), book.bids[k].vol] })
+    return new OrderBook(asks, bids, "BITTREX");
+}).then(console.log)
